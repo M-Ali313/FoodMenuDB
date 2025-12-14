9ed3f6a62b49d5ccd9532a7ca2b8e0b8ddc2dd83
Ali <mohammadalihaidary77350@gmail.com>
2025-12-14 11:55:33 +0430

    I create file model and connect to the database

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
-app.listen(3000)
+
+const URLdb = 'mongodb+srv://Ali:Ali1234@cluster0.5t1pkto.mongodb.net/FoodMenuDB?appName=Cluster0'
+
+mongoose.connect(URLdb)
+.then((resutl)=> {
+        app.listen(3000)
+        console.log("Database connect")
+    })
+    .catch((err)=> console.log("Not connect "))
  
 app.get('/', (req, res)=>{
     res.send('Hello node.js')
